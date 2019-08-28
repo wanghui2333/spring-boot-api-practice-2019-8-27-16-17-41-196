c6edb096e7bc2db5b291f263042a560a8bd69467
wanghui <793147654@qq.com>
2019-08-28 22:00:30 +0800

    创建Company类

---
+++ src/main/java/com/tw/apistackbase/model/Company.java
@@ -0,0 +1,54 @@
+package com.tw.apistackbase.model;
+
+import java.util.List;
+
+public class Company {
+	private int id;
+	private String campanyName;
+	private int employeeNumber;
+	private List<Employee> employees;
+
+	public Company(int id, String campanyName, int employeeNumber, List<Employee> employees) {
+		this.id = id;
+		this.campanyName = campanyName;
+		this.employeeNumber = employeeNumber;
+		this.employees = employees;
+	}
+
+	public Company() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCampanyName() {
+		return campanyName;
+	}
+
+	public void setCampanyName(String campanyName) {
+		this.campanyName = campanyName;
+	}
+
+	public int getEmployeeNumber() {
+		return employeeNumber;
+	}
+
+	public void setEmployeeNumber(int employeeNumber) {
+		this.employeeNumber = employeeNumber;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+
+
+}
